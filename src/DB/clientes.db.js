c747aeda251d2bac2a28e9f5888213c346c855db
priscilabgmoya <105519676+priscilabgmoya@users.noreply.github.com>
2023-10-19 23:27:26 -0300

    se agrega get de empleados

--- src/DB/clientes.db.js
+++ src/DB/clientes.db.js
@@ -1,9 +1,9 @@
-const connection = require('./connection.db');
+const { getConnection } = require('./connection.db');
 const tableEmployee = "empleado"; 
 module.exports.getClientes = async function() {
  let conn;
     try {
-        conn = await connection.getConnection(); 
+        conn = await getConnection(); 
         const SQL = `SELECT  nombre, apellido, correo_electronico FROM ${tableEmployee}`
         let rows = await conn.query(SQL); 
         return rows; 
